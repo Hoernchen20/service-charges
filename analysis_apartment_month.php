285e100f676f93349d8bcccec03f06248735e08a
Hoernchen20 <felix@horn-witten.de>
2017-12-15 18:19:48 +0100

    HTML Header und Top Navigation in PHP Funktion ausgegliedert

--- analysis_apartment_month.php
+++ analysis_apartment_month.php
@@ -1,56 +1,14 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-  <head>
-    <title>Nebenkostenabrechnung - Wohnung pro Monat</title>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="author" content="Felix Horn">
-    <meta http-equiv="language" content="de">
-    <link rel="stylesheet" media="screen" type="text/css" href="styles.css">
-    <link rel="stylesheet" media="print" type="text/css" href="printer.css">
-    <script type="text/JavaScript" src="inc/menue.inc.js"></script>
-  </head>
-  <body>
-    <div class="head">
-      <h1>Nebenkostenabrechnung</h1>
-    </div>
-    <div class="topnavi">
-      <ul>
-        <li>
-          <h3>Verwalten</h3>
-          <ul class="subnavi">
-            <li><a href="house.php">Haus</a></li>
-            <li><a href="apartment.php">Wohnung</a></li>
-            <li><a href="tenant.php">Mieter</a></li>
-            <li><a href="costs_house.php">Kosten pro Haus</a></li>
-            <li><a href="costs_person.php">Kosten pro Person</a></li>
-            <li><a href="costs_tenant.php">Kosten pro Mieter</a></li>
-            <li><a href="payment.php">Zahlungen</a></li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <h3>Erfassen</h3>
-          <ul class="subnavi">
-            <li>test</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <h3>Auswerten</h3>
-          <ul class="subnavi">
-<!--            <li><a href="analysis_tenant_month.php">Mieter pro Monat</a></li> -->
-            <li><a href="analysis_apartment_month.php">Wohnung pro Monat</a></li>
-            <li><a href="analysis_apartment_year.php">Wohnung pro Jahr</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+<?php
+  include 'inc/dbconnect.inc.php';
+  include 'inc/html_functions.inc.php';
+  include 'inc/php_functions.inc.php';
+  
+  PrintHtmlHeader("Nebenkostenabrechnung - Auswertung");
+  PrintHtmlTopNavigation();
+?>
     <div class="inhalt">
     <?php
-      include 'inc/dbconnect.inc.php';
-      include 'inc/php_functions.inc.php';
+      
       
       $costs_diff = 0;
       $costs_diff_month = array_fill(1, 12, 0);
--- tenant.php
+++ tenant.php
@@ -1,51 +1,10 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-  <head>
-    <title>Nebenkostenabrechnung - Mieter</title>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="author" content="Felix Horn">
-    <meta http-equiv="language" content="de">
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <script type="text/JavaScript" src="inc/menue.inc.js"></script>
-  </head>
-  <body>
-    <div class="head">
-      <h1>Nebenkostenabrechnung</h1>
-    </div>
-    <div class="topnavi">
-      <ul>
-        <li>
-          <h3>Verwalten</h3>
-          <ul class="subnavi">
-            <li><a href="house.php">Haus</a></li>
-            <li><a href="apartment.php">Wohnung</a></li>
-            <li><a href="tenant.php">Mieter</a></li>
-            <li><a href="costs_house.php">Kosten pro Haus</a></li>
-            <li><a href="costs_person.php">Kosten pro Person</a></li>
-            <li><a href="costs_tenant.php">Kosten pro Mieter</a></li>
-            <li><a href="payment.php">Zahlungen</a></li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <h3>Erfassen</h3>
-          <ul class="subnavi">
-            <li>test</li>
-          </ul>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <h3>Auswerten</h3>
-          <ul class="subnavi">
-<!--            <li><a href="analysis_tenant_month.php">Mieter pro Monat</a></li> -->
-            <li><a href="analysis_apartment_month.php">Wohnung pro Monat</a></li>
-            <li><a href="analysis_apartment_year.php">Wohnung pro Jahr</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
+<?php
+  include 'inc/dbconnect.inc.php';
+  include 'inc/html_functions.inc.php';
+  
+  PrintHtmlHeader("Nebenkostenabrechnung - Mieter");
+  PrintHtmlTopNavigation();
+?>
     <div class="inhalt">
       <h2>Verwalten - Mieter</h2>
       <?php
